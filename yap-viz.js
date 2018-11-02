0c74411154df06f75e1c9e720e1affc4b0fd340c
Danny Gershman <danny.gershman@gmail.com>
2018-11-02 00:21:05 -0400

    revert h3

--- yap-viz.js
+++ yap-viz.js
@@ -41,7 +41,7 @@ function setMapInfo(pos) {
     infoWindow.setPosition(pos);
     getServiceBodyForCoordinates(pos.lat, pos.lng, function(data) {
         var serviceBodyDetails = getServiceBodyById(data[0]["service_body_bigint"]);
-        var content = "<h3><b>" + serviceBodyDetails["name"] + "</b></h3>";
+        var content = "<b>" + serviceBodyDetails["name"] + "</b>";
         content += "<br>Helpline: <a href='tel:" + serviceBodyDetails["helpline"].split("|")[0] + "' target='_blank'>" + serviceBodyDetails["helpline"].split("|")[0] + "</a>";
         infoWindow.setContent(content);
         infoWindow.open(map);
